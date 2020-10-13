5dd8fa60e4167c778c7005bd6db7ab578111043d
Richie Black <rb15243@gmail.com>
2020-10-12 20:37:39 -0700

    added all fn to model

--- framework/BaseModel.php
+++ framework/BaseModel.php
@@ -6,10 +6,39 @@ class BaseModel
 {
     protected $db = null;
 
+    protected $table = null;
+
     public function __construct()
     {
         if (DatabaseConnection::$dbConnection) {
-            $this->db = clone DatabaseConnection::$dbConnection;    
+            $this->db = DatabaseConnection::$dbConnection;    
         }
+    }
+
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    public function all()
+    {
+        if (!$this->db || !$this->table) {
+            return false;
+        }
+
+        $results = [];
+        foreach ($this->db->query("SELECT * FROM $this->table") as $row) {
+            $row = array_filter($row, fn($key) => !is_numeric($key), ARRAY_FILTER_USE_KEY);
+            $model = new $this;
+
+            // todo: swap these from object properties to a custom wrapper.
+            foreach ($row as $columnName => $value) {
+                $model->$columnName = $value;
+            }
+
+            $results[] = $model;
+        }
+
+        return $results;
     }
 }
