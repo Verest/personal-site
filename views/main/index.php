5f50ea268a0f0bd1439cc6f6291c21eaddef6e2a
Richie Black <rb15243@gmail.com>
2020-10-15 20:44:38 -0700

    cleaned up html, added favicon, updated svg

--- views/main/index.php
+++ views/main/index.php
@@ -9,13 +9,11 @@
 
     <body>
         <header>
-            <!-- todo: header -->
-            <div class="title">
-            
+            <div class="container">
                 <h1>
                     <svg id="logo" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="8.485185451185714 6.674214553520983 291.167 291.16700000000003" width="266.67" height="266.67">
                         <defs>
-                            <path d="M282.65 290.34L15.99 290.34L15.99 23.67L272.78 23.67L272.78 223.67L188.04 223.67L143.47 157.01L188.04 157.01L188.04 90.34L102.44 90.34L102.44 234.83" id="d18dkHyrW6"></path>
+                            <path d="M282.65 290.34L15.99 290.34L15.99 23.67L272.78 23.67L272.78 213.67L188.04 213.67L143.47 157.01L188.04 157.01L188.04 85.34L102.44 85.34L102.44 224.83" id="d18dkHyrW6"></path>
                         </defs>
                         <g>
                             <use xlink:href="#d18dkHyrW6" opacity="1" fill-opacity="0" stroke="white" stroke-width="20" stroke-opacity="1"></use>
@@ -26,8 +24,6 @@
                     </span>
                 </h1>
             </div>
-
-            
         </header>
 
         <main class='container'>
@@ -36,7 +32,9 @@
 
 
         <footer>
+            <div class="container">
             Todo: footer content
+            </div>
         </footer>
 
         <script src="/js/app.js"></script>
