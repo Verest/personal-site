454a2a45ebb7a9ce999b60a74e37afce55600396
Richie Black <rb15243@gmail.com>
2020-10-16 18:43:55 -0700

    added starter homepage text

--- views/main/index.php
+++ views/main/index.php
@@ -27,7 +27,18 @@
         </header>
 
         <main class='container'>
-           <h1>About Me</h1>
+            <h1>About Me</h1>
+            <p>
+                I am a Full Stack Developer with a passion for personal development. I am constantly changing, growing, and 
+                finding new and better ways go move throughout my life. I am commonly learning new things, reading,
+                journaling, socializing, or engaging in men's work.
+            </p>
+
+            <h2>Site Purpose</h2>
+            <p>
+                The purpose of this site is largely to practice hosting on AWS, setting up my own server configurations, etc.
+                As always, this might change in the future.
+            </p>
         </main>
 
 
