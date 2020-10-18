980ff4b6a99747250a62961ca646e475a7e222ae
Richie Black <rb15243@gmail.com>
2020-10-17 20:10:23 -0700

    moved html to react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,48 @@
-console.log("JS was loaded...");
+import React from 'react';
+import { render } from 'react-dom';
+
+const HTML = () => {
+    return [
+        <header>
+            <div class="container">
+                <h1>
+                    <svg id="logo" version="1.1" viewBox="8.485185451185714 6.674214553520983 291.167 291.16700000000003"
+                        stroke="white" stroke-width="25px" fill="none">
+                            
+                        <path d="M282.65 290.34L15.99 290.34L15.99 23.67L272.78 23.67L272.78 213.67L188.04 213.67L143.47 157.01L188.04 157.01L188.04 85.34L102.44 85.34L102.44 224.83" id="d18dkHyrW6"></path>
+                    </svg>
+
+                    <span>
+                        <span class="hidden">R</span>ichie Black
+                    </span>
+                </h1>
+            </div>
+        </header>,
+
+        <main class='container'>
+            <h1>About Me</h1>
+            <p>
+                I am a Full Stack Developer with a passion for personal development. I am constantly changing, growing, and 
+                finding new and better ways go move throughout my life. I am commonly learning new things, reading,
+                journaling, socializing, or engaging in men's work.
+            </p>
+
+            <h2>Site Purpose</h2>
+            <p>
+                The purpose of this site is largely to practice hosting on AWS, setting up my own server configurations, etc.
+                As always, this might change in the future.
+            </p>
+        </main>,
+
+        <footer>
+            <div class="container">
+            Todo: footer content
+            </div>
+        </footer>
+    ];
+};
+
+render(
+    <HTML />,
+    document.getElementById('root')
+);
--- views/main/index.php
+++ views/main/index.php
@@ -8,45 +8,7 @@
     </head>
 
     <body>
-        <header>
-            <div class="container">
-                <h1>
-                    <svg id="logo" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="8.485185451185714 6.674214553520983 291.167 291.16700000000003" width="266.67" height="266.67">
-                        <defs>
-                            <path d="M282.65 290.34L15.99 290.34L15.99 23.67L272.78 23.67L272.78 213.67L188.04 213.67L143.47 157.01L188.04 157.01L188.04 85.34L102.44 85.34L102.44 224.83" id="d18dkHyrW6"></path>
-                        </defs>
-                        <g>
-                            <use xlink:href="#d18dkHyrW6" opacity="1" fill-opacity="0" stroke="white" stroke-width="20" stroke-opacity="1"></use>
-                        </g>
-                    </svg>
-                    <span>
-                        <span class="hidden">R</span>ichie Black
-                    </span>
-                </h1>
-            </div>
-        </header>
-
-        <main class='container'>
-            <h1>About Me</h1>
-            <p>
-                I am a Full Stack Developer with a passion for personal development. I am constantly changing, growing, and 
-                finding new and better ways go move throughout my life. I am commonly learning new things, reading,
-                journaling, socializing, or engaging in men's work.
-            </p>
-
-            <h2>Site Purpose</h2>
-            <p>
-                The purpose of this site is largely to practice hosting on AWS, setting up my own server configurations, etc.
-                As always, this might change in the future.
-            </p>
-        </main>
-
-
-        <footer>
-            <div class="container">
-            Todo: footer content
-            </div>
-        </footer>
+        <div id="root"></div>
 
         <script src="/js/app.js"></script>
     </body>
