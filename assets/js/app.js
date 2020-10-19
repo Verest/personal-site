6df1f132622422b54a77a0b4f1b714148f21a89a
Richie Black <rb15243@gmail.com>
2020-10-18 20:40:23 -0700

    extracted header and footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 
 const HTML = () => {
     return [
-        <header>
-            <div class="container">
-                <h1>
-                    <svg id="logo" version="1.1" viewBox="8.485185451185714 6.674214553520983 291.167 291.16700000000003"
-                        stroke="white" stroke-width="25px" fill="none">
-                            
-                        <path d="M282.65 290.34L15.99 290.34L15.99 23.67L272.78 23.67L272.78 213.67L188.04 213.67L143.47 157.01L188.04 157.01L188.04 85.34L102.44 85.34L102.44 224.83" id="d18dkHyrW6"></path>
-                    </svg>
-
-                    <span>
-                        <span class="hidden">R</span>ichie Black
-                    </span>
-                </h1>
-            </div>
-        </header>,
+        <Header />,
 
         <main class='container'>
             <h1>About Me</h1>
@@ -34,11 +22,7 @@ const HTML = () => {
             </p>
         </main>,
 
-        <footer>
-            <div class="container">
-            Todo: footer content
-            </div>
-        </footer>
+       <Footer />
     ];
 };
 
---
+++ assets/js/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div class="container">
+            Todo: footer content
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
